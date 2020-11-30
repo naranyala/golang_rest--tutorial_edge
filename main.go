f73a1071f8f6f355dbe7241a9fe8cedd6d5eaf00
naranyala <fudzer.mh@gmail.com>
2020-11-30 17:45:47 +0700

    (+) main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
+
+func main() {
+	handleRequest()
+}
